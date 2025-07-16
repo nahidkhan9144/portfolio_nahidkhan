bf9905d522bed81633737a1a4364cc9dd3e16f59
nahidkhan9144 <nahidkahn9144@gmail.com>
2025-07-16 22:50:30 +0530

    chane home and about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,8 +6,10 @@ function page() {
       <div className='row mt-4 p-3 w-full border-b border-blue-100 shadow-xl rounded-tl-sm hover:bg-blue-50'>
         <h2 className='text-center'>about page</h2>
       </div>
-      <div className='w-80 row mt-4 p-3 border-b border-blue-100 shadow-xl rounded-tl-sm hover:bg-blue-50'>
-     I’m a web and mobile developer with 2.4 years of experience, proficient in PHP, Laravel, React.js, AngularJS, HTML5, CSS3, and JavaScript, with strong skills in building responsive applications, integrating RESTful APIs, and adapting quickly to new tools and technologies.
+      <div className='pl-52 pr-52 '>
+        <div className='row mt-4 p-3 border-b border-blue-100 shadow-xl rounded-tl-sm hover:bg-blue-50'>
+          I’m a web and mobile developer with 2.4 years of experience, proficient in PHP, Laravel, React.js, AngularJS, HTML5, CSS3, and JavaScript, with strong skills in building responsive applications, integrating RESTful APIs, and adapting quickly to new tools and technologies.
+        </div>
       </div>
     </div>
   )

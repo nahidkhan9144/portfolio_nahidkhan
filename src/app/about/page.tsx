e94a08de8cf072b1f96134ac89c0d4972203f4ed
nahidkhan9144 <nahidkahn9144@gmail.com>
2025-07-16 22:45:18 +0530

    chane home and about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='flex justify-center items-center'>
-      <div className='mt-4 p-3 w-72 border-b border-blue-100 shadow-xl rounded-tl-sm hover:bg-blue-50'>
+    <div className='flex flex-wrap justify-center'>
+      <div className='row mt-4 p-3 w-full border-b border-blue-100 shadow-xl rounded-tl-sm hover:bg-blue-50'>
         <h2 className='text-center'>about page</h2>
+      </div>
+      <div className='w-80 row mt-4 p-3 border-b border-blue-100 shadow-xl rounded-tl-sm hover:bg-blue-50'>
+     I’m a web and mobile developer with 2.4 years of experience, proficient in PHP, Laravel, React.js, AngularJS, HTML5, CSS3, and JavaScript, with strong skills in building responsive applications, integrating RESTful APIs, and adapting quickly to new tools and technologies.
       </div>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,25 @@ import React from 'react'
 
 function page() {
   return (
-    <main className="mt-3">
-      <div className="flex justify-center">
-        <h1 className='p-4 rounded-xl border border-sky-200'>its nahid</h1>
+    <main className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+      <h2 className='mb-5'><b>Skills</b></h2>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        <h1 className='p-4 rounded-xl border border-sky-200'>PHP</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>Laravel</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>AngularJS</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>React.js</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>Flutter</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>Dart</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>JavaScript</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>HTML</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>CSS</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>Bootstrap</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>Tailwind CSS</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>jQuery</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>AJAX</h1>
+        <h1 className='p-4 rounded-xl border border-sky-200'>API Integration</h1>
       </div>
+
     </main>
   );
 }

aaec4ed07f38cac0de8187df9b63795f93a21c00
nahidkhan9144 <nahidkahn9144@gmail.com>
2025-07-21 22:23:10 +0530

    chane home and about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,6 +10,9 @@ function page() {
         <div className='row mt-4 p-3 border-b border-blue-100 shadow-xl rounded-tl-sm hover:bg-blue-50'>
           I’m a web and mobile developer with 2.4 years of experience, proficient in PHP, Laravel, React.js, AngularJS, HTML5, CSS3, and JavaScript, with strong skills in building responsive applications, integrating RESTful APIs, and adapting quickly to new tools and technologies.
         </div>
+        <div className='row mt-4 p-3 border-b border-red-100 shadow-xl rounded-tl-sm hover:bg-red-50'>
+       •	Web and Mobile Developer with 2.4 years of industry experience. •	Proficient in building responsive and user-friendly web and mobile applications. •	Experienced with modern frameworks and technologies including PHP, Laravel, React.js, and AngularJS. •	Skilled in front-end development using HTML5, CSS3, and JavaScript. •	Strong background in integrating RESTful APIs and third-party services. •	Passionate about clean code, best practices, and delivering efficient solutions. •	Quick learner, adaptable to new tools, frameworks, and team environments. •	Committed to contributing to innovative and impactful projects.
+        </div>
       </div>
     </div>
   )

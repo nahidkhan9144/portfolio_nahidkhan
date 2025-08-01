033fdc54d6d50562801d006778fe301fc921545b
nahidkhan9144 <nahidkahn9144@gmail.com>
2025-08-01 22:49:19 +0530

    changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-500" style={{ padding: '1rem' }}>
+    <footer className="bg-gray-800" style={{ padding: '1rem' }}>
       <p className="text-center">© 2025 My Footer Components ❤️</p>
     </footer>
   );

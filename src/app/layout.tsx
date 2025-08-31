c87867b841b09b2a6ae67c7545613aa5b8bcac40
nahidkhan9144 <nahidkahn9144@gmail.com>
2025-08-31 23:29:11 +0530

    change background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import { FlickeringGrid } from "@/components/magicui/flickering-grid";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -22,19 +22,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} ${inter.variable} antialiased`}
-      >
+      <body className={`${inter.variable} antialiased`}>
+        <div className="relative min-h-screen flex flex-col">
+          {/* Background */}
+          <FlickeringGrid
+            className="fixed inset-0 -z-10 w-full h-full"
+            squareSize={4}
+            gridGap={6}
+            color="#5c718a"
+            // color="#60A5FA"
+            maxOpacity={0.5}
+            flickerChance={0.1}
+          />
 
-        <div className='flex flex-col h-screen'>
-
+          {/* Foreground content */}
           <Header />
-          <main className="main-content">
+          <main className="main-content flex-1">
             {children}
           </main>
           <Footer />
         </div>
       </body>
+
+
     </html>
   );
 }
